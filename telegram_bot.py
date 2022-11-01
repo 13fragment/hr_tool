f1990b9f2c4e9c9d8727a403819ca9d2a2ce623f
4kith <mgolubevbiz@gmail.com>
2022-11-01 23:18:15 +0300

    tries to multiple 3 functions

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,8 +4,21 @@ from aiogram import Dispatcher,executor,types,Bot
 API_TOKEN ='5727921189:AAHSWpPnpEWgjJYRVsEUzBGhi_HgTF8Kit8'
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+@dp.message_handler(commands=['start'])
+async def welcome_message(message:types.Message):
+    await message.reply('Welcome to the party!')
+@dp.message_handler(commands=['start_bot'])
+async def start_bot(message:types.BotCommand):
+    await executor.start_polling(dp,skip_updates='True')
+@dp.message_handler(commands=['stop'])
+async def stop_bot(message:types.BotCommand):
+    await dp.stop_polling()
 @dp.message_handler()
 async def send_welcome(message:types.Message):
-    await message.reply(message.text)
+    await message.answer(message.text)
+    global a
+    a = message.text
+    print(a)
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp,skip_updates='True')
+
