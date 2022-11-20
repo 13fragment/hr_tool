f3375687ea77d168da888aab76bec195f247a626
4kith <mgolubevbiz@gmail.com>
2022-11-21 01:04:43 +0300

    add greeting
    Greeting is test version

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,24 +1,26 @@
 from aiogram import Dispatcher,executor,types,Bot
-# from aiogram.types import KeyboardButton так импортируются отдельные типы, например тип с кнопкой 
+from aiogram.types import KeyboardButton,ReplyKeyboardMarkup
+
 # import logging библиотечка с логами, для дальнейшего релиза будет полезна
 API_TOKEN ='5727921189:AAHSWpPnpEWgjJYRVsEUzBGhi_HgTF8Kit8'
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
-@dp.message_handler(commands=['start'])
-async def welcome_message(message:types.Message):
-    await message.reply('Welcome to the party!')
-@dp.message_handler(commands=['start_bot'])
-async def start_bot(message:types.BotCommand):
-    await executor.start_polling(dp,skip_updates='True')
-@dp.message_handler(commands=['stop'])
-async def stop_bot(message:types.BotCommand):
-    await dp.stop_polling()
-@dp.message_handler()
-async def send_welcome(message:types.Message):
-    await message.answer(message.text)
-    global a
-    a = message.text
-    print(a)
+
+text = open('greeting_msg.txt',encoding='utf-8')
+if text != None:
+    data = text.read()
+
+commands_welcome = ['start','начать','Начать', 'НАЧАТЬ','Start']
+
+@dp.message_handler (commands=commands_welcome)
+async def greeting(message:types.Message):
+    await bot.send_message(chat_id=message.from_user.id, text = data,parse_mode="HTML")
+    await message.delete()
+
+
+
+@dp.message_handler(commands=['help','помощь'])
+async def help_command(message:types.Message):
+    await message.reply('Связь с тех поддержкой: @mgo1ubev')
 if __name__ == '__main__':
     executor.start_polling(dp,skip_updates='True')
-
