2882819b1107b5f9d82c6c25294686ac1dfc1674
4kith <mgolubevbiz@gmail.com>
2022-11-21 02:14:31 +0300

    add buttons: rand mp + gen mp

--- telegram_bot.py
+++ telegram_bot.py
@@ -6,21 +6,36 @@ API_TOKEN ='5727921189:AAHSWpPnpEWgjJYRVsEUzBGhi_HgTF8Kit8'
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
+
 text = open('greeting_msg.txt',encoding='utf-8')
+commands_welcome = ['start','начать','Начать', 'НАЧАТЬ','Start']
 if text != None:
     data = text.read()
+    
 
-commands_welcome = ['start','начать','Начать', 'НАЧАТЬ','Start']
+keyboard_mp = ReplyKeyboardMarkup (keyboard=[
+    [
+    KeyboardButton(text='Создать мероприятие'),
+    KeyboardButton(text = 'Сгенерировать мероприятие')
+    ]
+ ],
+ resize_keyboard=True, input_field_placeholder='Выберите действие' + ' ' + '⬇️')
+
 
 @dp.message_handler (commands=commands_welcome)
 async def greeting(message:types.Message):
-    await bot.send_message(chat_id=message.from_user.id, text = data,parse_mode="HTML")
+    await bot.send_message(chat_id=message.from_user.id, text = data,parse_mode="HTML",reply_markup=keyboard_mp)
     await message.delete()
-
-
+    if message.text == 'Сгенерировать мероприятие':
+       await message.reply('ok')
 
 @dp.message_handler(commands=['help','помощь'])
 async def help_command(message:types.Message):
     await message.reply('Связь с тех поддержкой: @mgo1ubev')
+
+
+
+
+
 if __name__ == '__main__':
     executor.start_polling(dp,skip_updates='True')
